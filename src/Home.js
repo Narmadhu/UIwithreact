9f15331ea0ed6339ed52de1e70fb6310aba1d0f5
Narmadhu <oknarmadhu@gmail.com>
2020-09-05 13:13:28 +0530

    added basic html structure

---
+++ src/Home.js
@@ -0,0 +1,250 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home grid-2">
+      <div className="gird1">
+        <h2>Edukated</h2>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Discover</li>
+            <li>Messages</li>
+            <li>Settings</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Updated Courses</h3>
+          <h4>by Narmadhu</h4>
+        </div>
+        <div>
+          <h3>1800</h3>
+          <h4>points</h4>
+        </div>
+        <div>
+          <h3>45.3%</h3>
+          <h4>Completed</h4>
+        </div>
+        <div>
+          <h3>Alex Dixon</h3>
+          <h4>alex</h4>
+        </div>
+      </div>
+      <div className="grid2">
+        <div className="grid-2">
+          <div className="streaming">
+            <ul>
+              <li>Streaming Now</li>
+              <li>...</li>
+            </ul>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+          <div className="search">
+            <ul>
+              <li>Advanced search</li>
+              <li>...</li>
+            </ul>
+            <input type="text" placeholder="start writing something" />
+            <button>Find</button>
+          </div>
+        </div>
+        <div className="popular">
+          <ul>
+            <li>Popular this week</li>
+            <li>...</li>
+          </ul>
+          <div>
+            <div className="popular-each">
+              <img src="" alt="" />
+              <ul>
+                <li>
+                  <span>Lana Marandina</span>
+                </li>
+                <li>
+                  <span>lana </span>
+                </li>
+              </ul>
+              <button>54 min</button>
+              <div className="description">
+                <h3>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolor corrupti quas facilis ad optio animi repellendus, eum
+                  exercitationem, fugit accusamus omnis odit cumque voluptas at
+                  debitis, ex quasi quibusdam. Et.
+                </h3>
+              </div>
+            </div>
+            <div className="popular-each">
+              <div>
+                <img src="" alt="" />
+                <ul>
+                  <li>
+                    <span>Lana Marandina</span>
+                  </li>
+                  <li>
+                    <span>lana </span>
+                  </li>
+                </ul>
+                <button>54 min</button>
+                <div className="description">
+                  <h3>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor corrupti quas facilis ad optio animi repellendus, eum
+                    exercitationem, fugit accusamus omnis odit cumque voluptas
+                    at debitis, ex quasi quibusdam. Et.
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="popular-each">
+              <div>
+                <img src="" alt="" />
+                <ul>
+                  <li>
+                    <span>Lana Marandina</span>
+                  </li>
+                  <li>
+                    <span>lana </span>
+                  </li>
+                </ul>
+                <button>54 min</button>
+                <div className="description">
+                  <h3>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor corrupti quas facilis ad optio animi repellendus, eum
+                    exercitationem, fugit accusamus omnis odit cumque voluptas
+                    at debitis, ex quasi quibusdam. Et.
+                  </h3>
+                </div>
+              </div>
+              <div className="popular-each">
+                <div>
+                  <img src="" alt="" />
+                  <ul>
+                    <li>
+                      <span>Lana Marandina</span>
+                    </li>
+                    <li>
+                      <span>lana </span>
+                    </li>
+                  </ul>
+                  <button>54 min</button>
+                  <div className="description">
+                    <h3>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Dolor corrupti quas facilis ad optio animi repellendus,
+                      eum exercitationem, fugit accusamus omnis odit cumque
+                      voluptas at debitis, ex quasi quibusdam. Et.
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <div className="popular-each">
+                <div>
+                  <img src="" alt="" />
+                  <ul>
+                    <li>
+                      <span>Lana Marandina</span>
+                    </li>
+                    <li>
+                      <span>lana </span>
+                    </li>
+                  </ul>
+                  <button>54 min</button>
+                  <div className="description">
+                    <h3>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Dolor corrupti quas facilis ad optio animi repellendus,
+                      eum exercitationem, fugit accusamus omnis odit cumque
+                      voluptas at debitis, ex quasi quibusdam. Et.
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid-2">
+              <div className="tutor">
+                <ul>
+                  <li>
+                    <span> Top Tutors </span>
+                  </li>
+                  <li>
+                    <span> ... </span>
+                  </li>
+                </ul>
+                <div>
+                  <div className="tutor-each ">
+                    <img src="" alt="" />
+                    <ul>
+                      <li>
+                        <span>Melina</span>
+                      </li>
+                      <li>
+                        <span> by Melina</span>
+                      </li>
+                    </ul>
+                    <button>+</button>
+                  </div>
+                  <div className="tutor-each">
+                    <img src="" alt="" />
+                    <ul>
+                      <li>
+                        <span>Melina</span>
+                      </li>
+                      <li>
+                        <span> by Melina</span>
+                      </li>
+                    </ul>
+                    <button>+</button>
+                  </div>
+                  <div className="tutor-each">
+                    <img src="" alt="" />
+                    <ul>
+                      <li>
+                        <span>Melina</span>
+                      </li>
+                      <li>
+                        <span> by Melina</span>
+                      </li>
+                    </ul>
+                    <button>+</button>
+                  </div>
+                  <div className="tutor-each">
+                    <img src="" alt="" />
+                    <ul>
+                      <li>
+                        <span>Melina</span>
+                      </li>
+                      <li>
+                        <span> by Melina</span>
+                      </li>
+                    </ul>
+                    <button>+</button>
+                  </div>
+                </div>
+              </div>
+              <div className="offer">
+                <ul>
+                  <li>
+                    <span>Special offers</span>
+                  </li>
+                  <li>
+                    <span>...</span>
+                  </li>
+                </ul>
+                <div>
+                  <button>Learn more</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
